24cb3899c32cab1f608110efe528ffe4b662ae0b
Samuel Palacios <samuelpl0888@gmail.com>
2025-06-24 23:01:46 -0400

    trigger aumentar monto venta total

---
+++ scripts/procedures/trigger_update_venta_monto_total.sql
@@ -0,0 +1,39 @@
+-- Primero, eliminamos el trigger existente para poder modificar la función.
+DROP TRIGGER IF EXISTS trg_actualizar_monto_venta ON detalle_presentacion;
+
+-- Luego, eliminamos la función que utiliza el trigger.
+DROP FUNCTION IF EXISTS fn_actualizar_monto_total_venta();
+
+-- Ahora, creamos la función actualizada.
+CREATE OR REPLACE FUNCTION fn_actualizar_monto_total_venta()
+RETURNS TRIGGER AS $$
+DECLARE
+    v_venta_id INTEGER;
+BEGIN
+    /*
+     Determina el ID de la venta a actualizar.
+    */
+    IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+        v_venta_id := NEW.fk_venta;
+    END IF;
+
+    /*
+      Actualizar la venta con el nuevo monto total.
+    */
+    UPDATE venta
+    SET monto_total = (
+        SELECT COALESCE(SUM(cantidad * precio_unitario), 0)
+        FROM detalle_presentacion
+        WHERE fk_venta = v_venta_id
+    )
+    WHERE id = v_venta_id;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Crear el trigger cuando se inserta o actualiza un detalle de presentación.
+CREATE TRIGGER trg_actualizar_monto_venta
+AFTER INSERT OR UPDATE ON detalle_presentacion
+FOR EACH ROW
+EXECUTE FUNCTION fn_actualizar_monto_total_venta(); 
