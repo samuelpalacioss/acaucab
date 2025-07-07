ac5d0dbc3198f788b15bb294a0b96c0605456dfb
Samuel Palacios <samuelpl0888@gmail.com>
2025-07-07 18:53:00 -0400

    add: funcion para punto 1.1 ventas totales

---
+++ scripts/procedures/fn_get_total_generado_tienda.sql
@@ -0,0 +1,34 @@
+
+CREATE OR REPLACE FUNCTION fn_get_total_generado_tienda()
+RETURNS TABLE (
+    tienda_nombre VARCHAR,
+    tipo_tienda VARCHAR,
+    total_generado DECIMAL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    -- Ventas en tiendas físicas
+    SELECT
+        l.nombre AS tienda_nombre,
+        'Física' AS tipo_tienda,
+        SUM(v.monto_total) AS total_generado
+    FROM venta v
+    JOIN tienda_fisica tf ON v.fk_tienda_fisica = tf.id
+    JOIN lugar l ON tf.fk_lugar = l.id
+    GROUP BY l.nombre
+
+    UNION ALL
+
+    -- Ventas en tienda web
+    SELECT
+        tw.dominio_web AS tienda_nombre,
+        'Web' AS tipo_tienda,
+        SUM(v.monto_total) AS total_generado
+    FROM venta v
+    JOIN tienda_web tw ON v.fk_tienda_web = tw.id
+    GROUP BY tw.dominio_web;
+END;
+$$;
+
